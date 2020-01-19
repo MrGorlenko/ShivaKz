8deeee79aa836dd6f86daf6fd3b9312823326d19
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-01-19 18:35:38 +0300

    working slider

--- blocks/work/work__slider.js
+++ blocks/work/work__slider.js
@@ -1,8 +1,28 @@
 $(document).ready(function(){
+
+    var workModal = $('#work-modal'),
+     buttonWork = $('#button_work'),
+     buttonClose = $('#button_close'),
+     modalSubstrate = $('.work-modal__substrate'),
+     body = $('.body');
+
+
     $('#work-modal__slider').slick({
         infinite: true
-        // nextArrow: '<img src="../../images/right.png" alt="">',
-        // prevArrow: '<img src="../../images/left.png" alt="">'
     });
+
+
+    buttonWork.on('click', function(){
+        workModal.css('visibility', 'visible');
+        modalSubstrate.css('display', 'block');
+        body.css('overflow', 'hidden')
+    });
+
+    buttonClose.on('click', function(){
+        workModal.css('visibility', 'hidden');
+        modalSubstrate.css('display', 'none');
+        body.css('overflow', 'initial')
+    });
+    
 });
 
