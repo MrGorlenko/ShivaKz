4524ebcedd84cccb5b3f4e3204526e856116e39a
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-02-13 18:33:05 +0300

    trainings moduls

--- blocks/modal-window/modal-window.js
+++ blocks/modal-window/modal-window.js
@@ -1,12 +1,60 @@
 $(document).ready(function(){
-    var CloseModalWindow = $("#modal-window_close"),
+    var 
+    modalWindow__button_close = $('.modal-window__button_close'),
+
+    talents = $('#talents'),
+    talentsBtn = $('#talentsBtn'),
+    talentsModule = $('#talentsModule'),
+
     coach = $('#coach'),
     coachModule = $('#coachModule'),
     coachBtn = $('#coachBtn'),
+
+    facilitation = $('#facilitation'),
+    facilitationModule = $('#facilitationModule'),
+    facilitationBtn = $('#facilitationBtn'),
+
+    leaders = $('#leaders'),
+    leadersModule = $('#leadersModule'),
+    leadersBtn = $('#leadersBtn'),
     
     salesDep = $("#SalesDep"),
     salesDepBtn = $("#SalesDepBtn"),
-    salesDepModule = $("#SalesDepModule");
+    salesDepModule = $("#SalesDepModule"),
+
+    
+    valuesBtn = $('#valuesBtn'),
+    valuesModule = $('#valuesModule');
+
+    talents.on('click', function(){
+        talentsModule.css('display', 'flex');
+    })
+
+    talentsBtn.on('click', function(){
+        talentsModule.css('display', 'flex');
+        talentsModule.css('z-index', '150');
+
+    })
+
+    facilitation.on('click', function(){
+        facilitationModule.css('display', 'flex');
+    })
+
+    facilitationBtn.on('click', function(){
+        facilitationModule.css('display', 'flex');
+        facilitationModule.css('z-index', '150');
+
+    })
+
+    leaders.on('click', function(){
+        leadersModule.css('display', 'flex');
+    })
+
+    leadersBtn.on('click', function(){
+        leadersModule.css('display', 'flex');
+        leadersModule.css('z-index', '150');
+
+    })
 
     coach.on('click', function(){
         coachModule.css('display', 'flex');
@@ -28,8 +76,13 @@ $(document).ready(function(){
 
     })
 
+    valuesBtn.on('click', function(){
+        valuesModule.css('display', 'flex');
+        valuesModule.css('z-index', '150');
+    })
 
-    CloseModalWindow.on('click', function(){
+
+    modalWindow__button_close.on('click', function(){
         $(".modal-window").css('display', 'none');
     })
 })
