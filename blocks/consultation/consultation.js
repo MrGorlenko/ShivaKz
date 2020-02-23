32be5437576fe6f2412856c6fad547bd99fa5002
MrGorlenko <52908127+MrGorlenko@users.noreply.github.com>
2020-02-23 13:59:10 +0300

    consultation modale

--- blocks/consultation/consultation.js
+++ blocks/consultation/consultation.js
@@ -1,5 +1,54 @@
 $(document).ready(function(){
+
+    var
+    consultationButtonClose = $(".consultation__button_close"),
+    consultationModal = $(".consultation__modal"),
+    
+    subconsciousButton = $("#subconsciousButton"),
+    subconsciousModale = $("#subconsciousModale"),
+
+    creedsButton = $("#creedsButton"),
+    creedsModale = $("#creedsModale"),
+
+    face = $("#face"),
+    work = $("#work"),
+    footer = $("#footer"),
+    experience = $("#experience"),
+    approach = $("#approach");
+
     $('#consultation-slider').slick({
 
     })
+
+
+    consultationButtonClose.on('click', function(){
+        consultationModal.css('display', 'none');
+        face.css('display', 'block');
+        work.css('display', 'block');
+        experience.css('display', 'block');
+        approach.css('display', 'block');
+        footer.css('display', 'block');
+    })
+
+    subconsciousButton.on('click', function(){
+        subconsciousModale.css('display', 'flex');
+        face.css('display', 'none');
+        work.css('display', 'none');
+        experience.css('display', 'none');
+        approach.css('display', 'none');
+        footer.css('display', 'none');
+    })
+
+
+    creedsButton.on('click', function(){
+        creedsModale.css('display', 'flex');
+        face.css('display', 'none');
+        work.css('display', 'none');
+        experience.css('display', 'none');
+        approach.css('display', 'none');
+        footer.css('display', 'none');
+    })
+
+
+
 })
